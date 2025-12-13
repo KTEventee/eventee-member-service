9357c823c143eb888308b353b098e23093a7c424
hyeonda02 <alceste951005@naver.com>
2025-12-14 00:04:52 +0900

    (refactor) common 모듈 버전 변경

--- src/main/java/eventee/server/member/MemberApplication.java
+++ src/main/java/eventee/server/member/MemberApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(eventee.server.common.aws.S3Config.class)
+//@Import(eventee.server.common.aws.S3AutoConfiguration.class)
 public class MemberApplication {
 
 	public static void main(String[] args) {
